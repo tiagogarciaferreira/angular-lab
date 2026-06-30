948306392bee8fe1d96235a8c16735cc9d8490da
Tiago Garcia Ferreira <tiagogarciaferreira@gmail.com>
2026-06-30 11:48:46 -0300

    feat: enhance CartService with quantity support, add computed totalItems, and display cart badge in header

--- src/app/cart/cart-service.ts
+++ src/app/cart/cart-service.ts
@@ -1,5 +1,6 @@
-import {Injectable, signal} from '@angular/core';
+import {computed, Injectable, signal} from '@angular/core';
 import {Product} from '../products/product';
+import {CartItem} from './cart-item';
 
 @Injectable({
   providedIn: 'root',
@@ -7,9 +8,21 @@ import {Product} from '../products/product';
 
 export class CartService {
 
-  private readonly cartItems = signal<Product[]>([]);
+  private readonly cartItems = signal<CartItem[]>([]);
+
+  readonly totalItems = computed(() => this.cartItems()
+    .reduce((total, item) => total + item.quantity, 0)
+  );
 
   addToCart(product: Product){
-    this.cartItems.update((items) => [...items, product]);
+    this.cartItems.update((items) => {
+      const existingItem = items.find((item) => item.product.id === product.id);
+
+      if (existingItem){
+        return items.map((item) => item.product.id === product.id ? {...item, quantity: item.quantity + 1} : item);
+      }
+
+      return [... items, {product, quantity: 1}]
+    });
   }
 }
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {CartService} from '../cart/cart-service';
+import {MatBadge} from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatBadge],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
+
 export class Header {
 
+  protected readonly cartService = inject(CartService);
 }
